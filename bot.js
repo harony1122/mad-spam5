8c4370b04895fc2ed6cf41388a47303cd902054a
harony1122 <45592826+harony1122@users.noreply.github.com>
2019-01-21 13:25:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("532669874212700171")
+let channel =     client.channels.get("536852233824763915")
 setInterval(function() {
 channel.send(`love you azooz`);
 }, 30)
